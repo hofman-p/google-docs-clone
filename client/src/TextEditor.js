e889568598a5a134547735688e27d7a4f4ebde1b
Pierre HOFMAN <pierre.hofman@gmail.com>
2021-05-01 04:30:26 +0200

    [Client ]Add socket.io and connect to server

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import { io } from 'socket.io-client';
 
 const TOOLBAR_MODULES = [
   [{ header: [1, 2, 3, 4, 5, 6, false] }],
@@ -16,6 +17,14 @@ const TOOLBAR_MODULES = [
 
 export default function TextEditor() {
   const [text, setText] = useState('');
+
+  useEffect(() => {
+    const socket = io('http://localhost:3001');
+    return () => {
+      socket.disconnect();
+    }
+  }, []);
+
   return (
     <div>
       <ReactQuill value={text} onChange={setText} modules={ { toolbar: TOOLBAR_MODULES} }></ReactQuill>
