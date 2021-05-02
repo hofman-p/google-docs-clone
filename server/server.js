ed0a32dd9fc982e1f6aeb84757493f222436a3f0
Pierre HOFMAN <pierre.hofman@gmail.com>
2021-05-02 05:05:06 +0200

    [Server] fix constant naming

--- server/server.js
+++ server/server.js
@@ -51,7 +51,7 @@ const findOrCreateDocument = async id => {
   if (id == null) return;
   const document = await Document.findById(id);
   if (document) return document;
-  return await Document.create({ _id: id, data: defaultValue });
+  return await Document.create({ _id: id, data: DEFAULT_VALUE });
 };
 
 connectToDb();
