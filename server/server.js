7aeb2026187c88c36b1870968afae7c4ae654af3
Pierre HOFMAN <pierre.hofman@gmail.com>
2021-05-02 04:57:07 +0200

    [Server] Add Document model definition into mongoose, add mongoose connection, possibility to find or create a document, and find by id and update a document when receiving socket instruction to save it

--- server/server.js
+++ server/server.js
@@ -1,3 +1,21 @@
+const mongoose = require('mongoose');
+const Document = require('./Document');
+
+const connectToDb = async () => {
+  try {
+    await mongoose.connect('mongodb://localhost/google-docs-clone', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true
+    })
+  } catch (e) {
+    console.error('Problem while connecting to database', e);
+  }
+}
+
+connectToDb();
+
 const io = require('socket.io')(3001, {
   cors: {
     origin: 'http://localhost:3000',
@@ -5,14 +23,35 @@ const io = require('socket.io')(3001, {
   }
 });
 
+const defaultValue = '';
+
 io.on('connection', socket => {
-  socket.on('get-document', documentId => {
-    const data = '';
+  socket.on('get-document', async documentId => {
+    let document = null;
+    try {
+      document = await findOrCreateDocument(documentId);
+    } catch (e) {
+      console.error('Problem while finding or creating a document', e);
+    }
     socket.join(documentId);
-    socket.emit('load-document', data);
+    socket.emit('load-document', document.data);
     socket.on('send-delta', delta => {
       socket.broadcast.to(documentId).emit('received-delta', delta);
     });
+    socket.on('save-document', async data => {
+      try {
+        await Document.findByIdAndUpdate(documentId, { data });
+      } catch (e) {
+        console.error('Problem while saving document', e);
+      }
+    })
+    console.log('Client connected');
   })
-  console.log('Client connected');
 });
+
+const findOrCreateDocument = async id => {
+  if (id == null) return;
+  const document = await Document.findById(id);
+  if (document) return document;
+  return await Document.create({ _id: id, data: defaultValue });
+};
