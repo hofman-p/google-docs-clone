6b299a5638841f45516c79aa59f8aa4eecfc6903
Pierre HOFMAN <pierre.hofman@gmail.com>
2021-05-02 04:02:36 +0200

    [Server] Add multiple document differenciation with different room for each document

--- server/server.js
+++ server/server.js
@@ -6,8 +6,13 @@ const io = require('socket.io')(3001, {
 });
 
 io.on('connection', socket => {
-  socket.on('send-delta', delta => {
-    socket.broadcast.emit('received-delta', delta);
+  socket.on('get-document', documentId => {
+    const data = '';
+    socket.join(documentId);
+    socket.emit('load-document', data);
+    socket.on('send-delta', delta => {
+      socket.broadcast.to(documentId).emit('received-delta', delta);
+    });
   })
   console.log('Client connected');
 });
